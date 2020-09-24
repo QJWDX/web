044df3265e2ed7a85e837b017b7f2037bcd0c55a
lizhixiong <lizhixiong@runone.com>
2020-09-24 17:57:46 +0800

    新增登录日志文件

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\UserRequest;
 use App\Models\Common\User;
 use App\Models\Common\UserRole;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
---
+++ app/Models/Common/LoginLog.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Models\Common;
+
+
+use App\Models\BaseModel;
+
+class LoginLog extends BaseModel
+{
+    protected $table = 'login_log';
+    protected $guarded = [];
+}
