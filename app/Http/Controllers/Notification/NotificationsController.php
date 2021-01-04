0806436c68512bb83d69e9103a243d6e5b379b58
lizhixiong <1131941061@qq.com>
2021-01-05 00:11:27 +0800

    文件修改

--- app/Http/Controllers/Notification/NotificationsController.php
+++ app/Http/Controllers/Notification/NotificationsController.php
@@ -1,14 +1,12 @@
 <?php
 
 
-namespace App\Http\Controllers\Backend;
+namespace App\Http\Controllers\Notification;
 
 
-use App\Events\sendNotification;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DelRequest;
-use App\Models\Common\Notifications;
-use App\Models\Common\systemNotifications;
+use App\Models\Notification\Notifications;
 use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
@@ -107,27 +105,5 @@ class NotificationsController extends Controller
         }
         $data = $notifications->notificationCountStatistics($notifiable_id, $type, $notifiable_type);
         return $this->success($data);
-    }
-
-
-    /**
-     * 创建系统通知
-     * @param Request $request
-     * @param systemNotifications $systemNotifications
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function createNotifications(Request $request, systemNotifications $systemNotifications){
-        $title = $request->get('title', false);
-        $content = $request->get('content', false);
-        if($title == false || $content == false){
-            return $this->error(500, '参数错误');
-        }
-        $sysNotifications = $systemNotifications->addSystemNotifications([$title, $content]);
-        if(!$sysNotifications){
-            return $this->error(500, '创建失败');
-        }
-        // 向所有用户发送系统消息通知
-        event(new sendNotification($sysNotifications));
-        return $this->success('创建成功');
     }
 }
